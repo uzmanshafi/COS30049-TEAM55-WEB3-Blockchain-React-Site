1bacbdc004e864c4616045b1224d14eda23d23eb
uzman <uzmanshafifassy@gmail.com>
2023-09-02 17:01:12 +1000

    created directories , pages and components

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'background-color': '#090801',
+      'primary-color': '#a5ee2f',
+      'secondary-color': '#08b113',
+      'text-color': '#fcf8d9',
+      'accent-color': '#262626',
+    },
     extend: {},
   },
   plugins: [],
