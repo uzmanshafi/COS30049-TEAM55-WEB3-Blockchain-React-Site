1fe135d7208ce93eea359c97c51b92ecbfe1ecff
uzman <uzmanshafifassy@gmail.com>
2023-10-11 02:43:55 +1100

    added database connectivity

--- my-project/src/components/navbar.js
+++ my-project/src/components/navbar.js
@@ -8,8 +8,11 @@ import SearchBar from './searchbar';
 const Header = () => {
   const [isOpen, setisOpen] = useState(false);
   const [isWalletConnected, setWalletConnected] = useState(false);
+  const [userWalletAddress, setUserWalletAddress] = useState("");
+  const [isHovering, setIsHovering] = useState(false);
 
-  const navigate = useNavigate(); // useHistory is replaced by useNavigate
+
+  const navigate = useNavigate();
 
   let Links = [
     { name: 'Home', link: '/' },
@@ -20,14 +23,55 @@ const Header = () => {
     Links.splice(1, 0, { name: 'Dashboard', link: '/dashboard' });
   }
 
-  const connectWallet = () => {
-    setWalletConnected(!isWalletConnected);
+  const connectWallet = async () => {
+    if (isWalletConnected) {
+      setWalletConnected(false);
+      setUserWalletAddress("");
+      return;
+    }
+
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const userAddress = accounts[0];
+        setUserWalletAddress(userAddress);
+        await saveUserAddress(userAddress);
+        setWalletConnected(true);
+      } catch (error) {
+        console.error("User denied account access");
+      }
+    } else {
+      console.error("Ethereum browser extension (like MetaMask) not detected");
+    }
   };
+
+
+  const saveUserAddress = async (address) => {
+    try {
+      const response = await fetch("http://localhost:8000/users/save_wallet_address/", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ address })
+      });
+      const data = await response.json();
+
+      if (data.status === "success") {
+        console.log("Address saved successfully!");
+      } else {
+        console.error("Error saving address");
+      }
+    } catch (error) {
+      console.error("API error:", error);
+    }
+  };
+
 
   const renderWalletAddress = (address) => {
     const start = address.substring(0, 6);
     const end = address.substring(address.length - 4);
-    return `${start}...${end}`;
+    return ` ${start}...${end}`;
   };
 
   let allPages = [
@@ -38,9 +82,9 @@ const Header = () => {
     { name: 'Submit Product', link: '/submit_product' },
     { name: 'Dashboard', link: '/dashboard' },
   ];
-  
+
   const performSearch = (query) => {
-    const matchedPage = allPages.find((page) => 
+    const matchedPage = allPages.find((page) =>
       page.name.toLowerCase().includes(query.toLowerCase())
     );
 
@@ -87,14 +131,17 @@ const Header = () => {
             <SearchBar onSearch={performSearch} />
           </li>
           <button
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
             onClick={connectWallet}
             className="btn -my-2 py-1 px-2 md:ml-8 rounded-md border-2 border-black md:static tracking-wide font-semibold"
           >
             <FontAwesomeIcon icon={faWallet} />
             {isWalletConnected
-              ? renderWalletAddress('0xae2Fc483527B8EF99EB5D9B44875F005ba1FaE13')
+              ? (isHovering ? " DISCONNECT" : renderWalletAddress(userWalletAddress))
               : ' Connect Wallet'}
           </button>
+
         </ul>
       </div>
     </div>
