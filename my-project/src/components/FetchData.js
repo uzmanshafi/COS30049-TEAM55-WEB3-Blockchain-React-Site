5519729ee6de1cb226688d55dd69b2595f9248bd
xzovy <104540186@student.swin.edu.au>
2023-10-15 00:06:48 +1100

    styling fixed for home page

--- my-project/src/components/FetchData.js
+++ my-project/src/components/FetchData.js
@@ -6,39 +6,50 @@ import { faEthereum } from '@fortawesome/free-brands-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
 function FetchData() {
-    const [records, setRecords] = useState([])
+    const [records, setRecords] = useState([]);
+    const [displayTrendingOnly, setDisplayTrendingOnly] = useState(false);
+    
     const navigate = useNavigate();
     const goToProductPage = () => {
         navigate('/product');
       };
+      
     useEffect(()=> {
         axios.get('http://127.0.0.1:8000/products/')
         .then(res => {setRecords(res.data)})
         .catch(err => console.log(err))
     })
+
+    const displayedProducts = displayTrendingOnly
+        ? records.filter(product => product.isTrending === 1)
+        : records;
+
     return (
-        <div>
-            {records.map((product, i ) => (
-            <div className="bg-primary-color rounded-md h-96 w-64 shadow-lg p-4 flex flex-col justify-between items-center m-2">
-                <img className="object-contain h-fit w-full shadow-inner" src={product.image_path} alt={product.image_path} />
-                <h1 className="text-xl font-bold">{product.product_name}</h1>
-                <h1>{product.category.toUpperCase()}</h1>
-                <div className="flex justify-between w-full items-center">
-                    <h2 className="text-lg font-bold tracking-widest flex items-center">
-                        <FontAwesomeIcon icon={faEthereum} /> {product.price} ETH
-                    </h2>
-                    <button
-                        onClick={goToProductPage}
-                        className="bg-secondary-color py-2 px-4 rounded-md shadow-md font-bold tracking-wider text-white border-2 border-white"
-                        >
-                        BUY NOW
-                    </button>
+        <div className="">
+            <div className="p-4 md:p-12 flex flex-wrap justify-center items-start">
+                {records.map((product, i ) => (
+                <div className="">
+                    <div className="bg-primary-color rounded-md h-96 w-64 shadow-lg p-4 flex flex-col justify-between items-center m-2">
+                        <img className="object-contain h-fit w-full shadow-inner" src={product.image_path} alt={product.image_path} />
+                        <h1 className="text-xl font-bold">{product.product_name}</h1>
+                        <h1 className="">{product.category.toUpperCase()}</h1>
+                        <div className="flex justify-between w-full items-center">
+                            <h2 className="text-lg font-bold tracking-widest flex items-center">
+                                <FontAwesomeIcon icon={faEthereum} /> {product.price} ETH
+                            </h2>
+                            <button
+                                onClick={goToProductPage}
+                                className="bg-secondary-color py-2 px-4 rounded-md shadow-md font-bold tracking-wider text-white border-2 border-white"
+                                >
+                                BUY NOW
+                            </button>
 
+                        </div>
+                    </div>
                 </div>
+                ))}
             </div>
-            ))}
         </div>
-
         
     )
 }
--- my-project/src/pages/home.js
+++ my-project/src/pages/home.js
@@ -1,33 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import homeImage from '../images/home-image1.png';
-import ProductCard from '../components/product-card';
-import { NavLink } from 'react-router-dom';
-import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEthereum } from '@fortawesome/free-brands-svg-icons';
+import FetchData from './../components/FetchData'; // Import the FetchData component
 
 const Home = () => {
-    const [activeTab, setActiveTab] = useState("Trending");
-    const [productData, setProductData] = useState([]);
-  
-    useEffect(() => {
-      // Fetch product data from your API endpoint
-      axios.get('http://127.0.0.1:8000/products/')
-        .then((res) => {
-          setProductData(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }, []);
+  const [activeTab, setActiveTab] = useState("Trending");
 
-    const getTrendingProducts = () => {
-        return productData.filter((product) => product.isTrending == 1);
-      };
-  
-    const displayProducts = activeTab === "Trending" ? getTrendingProducts() : productData;
-
-    return (
-        <div className="grid grid-rows-[auto,1fr]">
-            <div className="w-full flex justify-center items-center py-6 md:py-10">
+  return (
+    <div className="grid grid-rows-[auto,1fr]">
+        <div className="w-full flex justify-center items-center py-6 md:py-10">
                 <div className="bg-primary-color px-4 py-6 md:p-10 rounded-md flex flex-col md:flex-row justify-between items-center md:w-1/2">
                     <div className="flex flex-col justify-center items-center mb-4 md:mb-0 md:w-4/5">
                         <h2 className="text-xs md:text-xl font-bold mb-2">BUY THE LATEST</h2>
@@ -45,31 +27,29 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div className=" w-full flex flex-col items-start">
-                <div className="flex justify-start self-center px-4 md:px-10">
-                    <button
-                        className={`px-6 py-2 rounded-full m-4 uppercase font-semibold shadow-md ${activeTab === "Trending" ? "bg-primary-color" : "bg-gray-600"}`}
-                        onClick={() => setActiveTab("Trending")}
-                    >
-                        Trending
-                    </button>
-                    <button
-                        className={`px-6 py-2 rounded-full m-4 uppercase font-semibold shadow-md ${activeTab === "Recent" ? "bg-primary-color" : "bg-gray-600"}`}
-                        onClick={() => setActiveTab("Recent")}
-                    >
-                        Recent
-                    </button>
-                </div>
-                <div className="p-4 md:p-12 flex flex-wrap justify-center items-start">
-                    {displayProducts.map((product, index) => (
-                        <div className="m-2 md:m-4" key={product.item_id}>
-                            <ProductCard />
-                        </div>
-                    ))}
-                </div>
-            </div>
+      <div className=" w-full flex flex-col items-start">
+        <div className="flex justify-start self-center px-4 md:px-10">
+          <button
+            className={`px-6 py-2 rounded-full m-4 uppercase font-semibold shadow-md ${
+              activeTab === "Trending" ? "bg-primary-color" : "bg-gray-600"
+            }`}
+            onClick={() => setActiveTab("Trending")}
+          >
+            Trending
+          </button>
+          <button
+            className={`px-6 py-2 rounded-full m-4 uppercase font-semibold shadow-md ${
+              activeTab === "Recent" ? "bg-primary-color" : "bg-gray-600"
+            }`}
+            onClick={() => setActiveTab("Recent")}
+          >
+            Recent
+          </button>
         </div>
-    );
+        <FetchData />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
